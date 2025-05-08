7662c41e87dc1a2882f621f3592768055278fe24
mihiranwijesekara <achinthamihiran654@gmail.com>
2025-05-08 22:22:43 +0530

    data

---
+++ lib/data/exercise_data.dart
@@ -0,0 +1,76 @@
+import 'package:workout/models/exercise_model.dart';
+
+class ExerciseData {
+  final List<Exercise> exerciseList = [
+    Exercise(
+      id: 0,
+      exerciseName: 'Push Up',
+      exerciseImageUrl: 'assets/exercises/push-ups.png',
+      noOfMinuites: 10,
+      completed: false,
+    ),
+    Exercise(
+      id: 1,
+      exerciseName: 'Squats',
+      exerciseImageUrl: 'assets/exercises/Squats.png',
+      noOfMinuites: 25,
+      completed: false,
+    ),
+    Exercise(
+      id: 2,
+      exerciseName: 'Pull Up',
+      exerciseImageUrl: 'assets/exercises/pulling-up.png',
+      noOfMinuites: 20,
+      completed: false,
+    ),
+    Exercise(
+      id: 3,
+      exerciseName: 'Plank',
+      exerciseImageUrl: 'assets/exercises/Plank.png',
+      noOfMinuites: 15,
+      completed: false,
+    ),
+    Exercise(
+      id: 4,
+      exerciseName: 'Leg Raises',
+      exerciseImageUrl: 'assets/exercises/Leg Raises.png',
+      noOfMinuites: 16,
+      completed: false,
+    ),
+    Exercise(
+      id: 5,
+      exerciseName: 'Burpees',
+      exerciseImageUrl: 'assets/exercises/Burpees.png',
+      noOfMinuites: 20,
+      completed: false,
+    ),
+    Exercise(
+      id: 6,
+      exerciseName: 'Sit Ups',
+      exerciseImageUrl: 'assets/exercises/Sit Ups.png',
+      noOfMinuites: 30,
+      completed: false,
+    ),
+    Exercise(
+      id: 7,
+      exerciseName: 'Jumping Jacks',
+      exerciseImageUrl: 'assets/exercises/Jumping Jacks.png',
+      noOfMinuites: 20,
+      completed: false,
+    ),
+    Exercise(
+      id: 8,
+      exerciseName: 'Mountain Climbers',
+      exerciseImageUrl: 'assets/exercises/Mountain Climbers.png',
+      noOfMinuites: 17,
+      completed: false,
+    ),
+    Exercise(
+      id: 9,
+      exerciseName: 'Bicycle Crunches',
+      exerciseImageUrl: 'assets/exercises/Bicycle Crunches.png',
+      noOfMinuites: 15,
+      completed: false,
+    ),
+  ];
+}
